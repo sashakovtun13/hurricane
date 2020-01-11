de14521bb0af93d093225cf035a30e8cac513827
Aleks <sashakovtun13@gmail.com>
2020-01-11 19:32:19 +0200

    up_0.02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 
   $(function(){
-    let height_txt=($(".black_txt_fire").css("height"))
-    $(selector).css(propertyName, value);
+    // let height_txt=($(".text_main").css("height"))
+    // $(selector).css(margin-top, value);
+  });
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+   
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
   });
 
