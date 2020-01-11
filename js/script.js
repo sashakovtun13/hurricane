d285c3ea286873b3c75046bd13703693aec78e24
Aleks <sashakovtun13@gmail.com>
2020-01-11 21:39:21 +0200

    up_0.05

--- js/script.js
+++ js/script.js
@@ -1,9 +1,50 @@
 
   $(function(){
+
+ //Центр текста
     let height_txt=($(".test").css("height"))
-    
     $(".test").css("padding-top", "calc(60vh - "+height_txt+"/2");
+
+
+
+
+
+
+//Скрол
+  
+  var ale_n=$(".ale_n");
+  var ale_n_b=$(".ale_n_b");
+  //ale_n.css("color", "red");
+  ale_n_b.css("background", "#C82929");
+  var block_2 = $(".block_2").offset();
+
+    // alert(block_2.top)
+    // alert(ale_n.top)
+    window.addEventListener('scroll', function() {    
+    if(ale_n.offset().top+10 > block_2.top){
+      ale_n_b.css("background", "#161616");
+      ale_n.css("color", "white");
+    }
+    else{
+      ale_n_b.css("background", "#C82929");
+      ale_n.css("color", "#000000");
+    }
   });
+
+
+    // alert(
+    //   ale_n.top
+    // )
+   
+ 
+
+
+
+
+
+
+
+//Плавный скрол
   $('a[href*="#"]').on('click', function (e) {
     e.preventDefault();
    
@@ -11,10 +52,6 @@
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
 
-
-
-
-    
-    
   });
+});
 
