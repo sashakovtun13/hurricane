62045fa40efdf8899458a37905424a690373c497
Aleks <sashakovtun13@gmail.com>
2020-01-11 20:33:41 +0200

    up_0.04

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
   $(function(){
-    // let height_txt=($(".text_main").css("height"))
-    // $(selector).css(margin-top, value);
+    let height_txt=($(".test").css("height"))
+    
+    $(".test").css("padding-top", "calc(60vh - "+height_txt+"/2");
   });
   $('a[href*="#"]').on('click', function (e) {
     e.preventDefault();
@@ -9,5 +10,11 @@
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
+
+
+
+
+    
+    
   });
 
